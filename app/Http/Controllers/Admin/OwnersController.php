2aee02c27c1bda0fe4970078fa747db62aeada27
saito4 <hiroto.saitou1201@gmail.com>
2024-12-09 10:27:31 +0900

    Store

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Owner;  //Eloquent エロクアント
 use Illuminate\Support\Facades\DB; //QueryBuilder クエリビルダー
+use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
 
 class OwnersController extends Controller
@@ -62,7 +63,23 @@ class OwnersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //$request->name;
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:owners'],
+            'password' => ['required', 'confirmed', 'min:8'],
+        ]);
+
+        Owner::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with('message', 'オーナー登録しました。');
+        
     }
 
     /**
