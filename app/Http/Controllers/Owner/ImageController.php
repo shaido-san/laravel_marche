ea6d6512130ad5029b0034e14f120a34349b727e
saito4 <hiroto.saitou1201@gmail.com>
2024-12-17 11:12:25 +0900

    image delete

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -8,6 +8,7 @@ use App\Models\Image;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\UploadImageRequest;
 use App\Services\ImageService;
+use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
@@ -112,6 +113,20 @@ class ImageController extends Controller
      */
     public function destroy($id)
     {
-        //
+        #削除処理をする前にストレージの中の画像を削除しないと、残ってしまう。
+        $image = Image::findOrFail($id);
+        $filePath = 'public/products/' . $image->filename;
+
+        #ここで念のため、ストレージを確認して、ファイルがあったら消す。
+        if(Storage::exists($filePath)){
+            Storage::delete($filePath);
+        }
+
+        Image::findOrFail($id)->delete(); 
+
+        return redirect()
+        ->route('owner.images.index')
+        ->with(['message' => '画像を削除しました。',
+        'status' => 'alert']);
     }
 }
